0e9c6e9c48e3c5b5de0b54ed8bc4d659ba457b4b
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-06-08 20:23:46 +0000

    updated info

--- styles/js/config.js
+++ styles/js/config.js
@@ -16,7 +16,7 @@ let charadex = {};
 /* ==================================================================== */
 charadex.site = {
   title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
+  url: "https://proutum-ann.github.io/gardenview-descension/",
   description: `A tool for organizing small ARPGs and species.`
 }
 
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1rJ-RA6nbq03-latCwjqyxj8RcMxfcPit7G3EZAnkB5k",
 
   pages: {
     masterlist:    "masterlist",
